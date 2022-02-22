ea7a078d7b70ee73d82b4c1e4035081eca1cbdf6
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-22 09:36:09 -0300

    Modifica módulos .js para .ts

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import{Negociacao} from './models/negociacao.js'; // importa o módulo Negociacao
 
-const negociacao = new Negociacao(new Date(), 10, 100); // cria instância de Negociacao
+const negociacao = new Negociacao(new Date()); // cria instância de Negociacao
 
 console.log(negociacao.volumeNegociacao);
